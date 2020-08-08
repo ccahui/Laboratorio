ffa2c4d3d4f89e4243c994e4ef498d88cc9cc8ea
Hristo Roque <aroquega@unsa.edu.pe>
2020-08-08 18:50:22 -0500

    Añadiendo mi avance

---
+++ Assets/Scenes/Scripting/DoomBall.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BallState
+{
+    calculatingTarget,
+    preparingToAttack,
+    attacking,
+    afterAttack,
+};
+
+[RequireComponent(typeof(Collider))]
+public class DoomBall : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    public Vector3 spawnPoint;
+    Vector3 lokOnTarget;
+    public BallState state;
+    public float speed = 10.0f;
+    public int reactionTime = 1;
+    private Collider collider;
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<Collider>();
+        rb = GetComponent<Rigidbody>();
+        spawnPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (state)
+        {
+            case BallState.calculatingTarget:
+                state = BallState.preparingToAttack;
+                StartCoroutine("PrepareToAttack");
+                break;
+            case BallState.preparingToAttack:
+                break;
+            case BallState.attacking:
+                if(Vector3.Distance(transform.position,lokOnTarget) > float.Epsilon)
+                {
+                    transform.position = Vector3.MoveTowards(transform.position,lokOnTarget,Time.deltaTime*speed);
+                } else
+                {
+                    state = BallState.calculatingTarget;
+                }
+                break;
+            case BallState.afterAttack:
+
+                break;
+        }
+    }
+
+    IEnumerator PrepareToAttack()
+    {
+        yield return new WaitForSeconds(reactionTime);
+        state = BallState.attacking;
+        lokOnTarget = target.position + Vector3.Normalize(target.position-transform.position)*2;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Doing dolor");
+            transform.position = spawnPoint;
+            state = BallState.calculatingTarget;
+        }
+    }
+}
---
+++ Assets/Scenes/Scripting/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    [SerializeField] Light lighting;
+
+    [SerializeField] GameObject openingScene;
+    Animator openingSceneAnimator;
+    [SerializeField] GameObject gameOverScreen;
+    Animator gameOverScreenAnimator;
+    [SerializeField] GameObject loadingScreen;
+
+    public int totalLifes = 5;
+    public int currentLifes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLifes = totalLifes;
+        openingSceneAnimator = openingScene.GetComponent<Animator>();
+        gameOverScreenAnimator = gameOverScreen.GetComponent<Animator>();
+
+        gameOverScreen.SetActive(false);
+        loadingScreen.SetActive(false);
+
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        openingSceneAnimator.SetBool("isOpen", true);
+
+    }
+
+    IEnumerator WaitForPlay()
+    {
+        yield return new WaitForSeconds(3);
+        openingSceneAnimator.SetBool("isOpen", false);
+    }
+
+    void LoseLife()
+    {
+        if (currentLifes > 0)
+        {
+            currentLifes -= 1;
+            lighting.intensity = (float)(currentLifes/totalLifes);
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Time.timeScale = 0;
+        gameOverScreen.SetActive(true);
+    }
+}
